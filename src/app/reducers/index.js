2e13364560a06617ef566a119d70834eab2a3ff4
perohrstrom <per.k.ohrstrom@gmail.com>
2016-12-05 10:02:32 -0500

    create Store with middle and import index reducer, after having combined reducers

---
+++ src/app/reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux'
+// import __NameofReducer__ from './nameofReducerFile' for each Reducer you create
+
+// const indexReducer = combineReducers({
+//  [name of state property]: [name of reducer which will handle its updates],
+//  friends (array): friendsReducer,
+//  posts: postsReducer
+// })
+
+const indexReducer = combineReducers({
+
+})
+
+export default indexReducer
